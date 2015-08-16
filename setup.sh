56b7692e2edc90d290a3f60485ddaac90d9e6543
Richard Dunks <richard@datapolitan.com>
2015-08-16 09:33:10 -0400

    updated sh files

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
 ssh -i /Users/richarddunks/Dropbox/Datapolitan/admin/datapolitan_com_virginia.pem ec2-user@52.20.51.98
+scp -i /Users/richarddunks/Dropbox/Datapolitan/admin/datapolitan_com_virginia.pem keys.py keys_boro.py ec2-user@52.20.51.98:citibike_dock_bot/
--- setup_ec2.sh
+++ setup_ec2.sh
@@ -3,3 +3,16 @@ yum install update
 yum groupinstall "Development Tools"
 yum install python-devel
 #don't install pip from another source
+
+#http://imperialwicket.com/aws-install-postgresql-on-amazon-linux-quick-and-dirty/
+sudo yum install postgresql postgresql-server postgresql-devel postgresql-contrib postgresql-docs
+sudo service postgresql initdb
+sudo vim /var/lib/pgsql9/data/pg_hba.conf
+sudo vim /var/lib/pgsql9/data/postgresql.conf
+sudo service postgresql start
+psql -U postgres
+
+pip install psycopg2
+
+#add password to pgpass
+chmod 0600 ~/.pgpass
