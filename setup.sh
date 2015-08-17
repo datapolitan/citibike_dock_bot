76850fe1a1958c1a467ef8020613c80b167d5f4c
Richard Dunks <richard@datapolitan.com>
2015-08-16 22:47:22 -0400

    cleanup files

--- setup.sh
+++
@@ -1,2 +0,0 @@
-ssh -i /Users/richarddunks/Dropbox/Datapolitan/admin/datapolitan_com_virginia.pem ec2-user@52.20.51.98
-scp -i /Users/richarddunks/Dropbox/Datapolitan/admin/datapolitan_com_virginia.pem keys.py keys_boro.py ec2-user@52.20.51.98:citibike_dock_bot/
